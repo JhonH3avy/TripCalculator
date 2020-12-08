e60ddee486ac3ea994e42d976c4618a641cf3a02
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2020-12-08 12:59:37 -0500

    stablish data context

--- TripCalculatorServer/Data/DataContext.cs
+++ TripCalculatorServer/Data/DataContext.cs
@@ -11,5 +11,13 @@ namespace Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<TripBag> TripBags { get; set; }
+
+        public DbSet<TripElement> TripElements { get; set; }
+
+        public DbSet<DayOfWork> DayOfWorks { get; set; }
     }
 }
