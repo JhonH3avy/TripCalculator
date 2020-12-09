2acd950d158f84f3a89fcaa24bab723b3a854514
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2020-12-09 18:13:02 -0500

    Link test to server

--- TripCalculatorTests/TripCalculationServiceTest.cs
+++ TripCalculatorTests/TripCalculationServiceTest.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace TripCalculatorTests
 {
-    public class UnitTest1
+    public class TripCalculationServiceTest
     {
         [Fact]
-        public void Test1()
+        public void CreateTwoTripBagsForTrip()
         {
 
         }
